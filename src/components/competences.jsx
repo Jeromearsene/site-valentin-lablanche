886127c7bcde0ad0bf40464d819a67f3a75ea449
Jerome <jeromearsene@gmail.com>
2025-01-26 20:15:37 +0100

    first commit

---
+++ src/components/competences.jsx
@@ -0,0 +1,47 @@
+export function Competences() {
+  return (
+    <div className="bg-primary text-custom-white p-3 inline-block m-4">
+      <h1 class="text-3xl pl-2 font-title">Compétences</h1>
+      <div class="mx-2 p-4 border-secondary border-l">
+        <h2 class="font-title text-xl mb-2 text-secondary">
+          Interprétation scénique
+        </h2>
+        <ul class="list-disc pl-4">
+          <li>Soliste lors de spectacles ()</li>
+          <li>Scènes ouvertes</li>
+          <li>Spectacle de chansons françaises ()</li>
+          <li>
+            Prix de composition/interprétation (Festival "La bastide qui
+            chante", Castillonnès 2012)
+          </li>
+          <li>Théatre + théatre d'impro (compagnie de théatre)</li>
+        </ul>
+        <h2 class="font-title text-xl  mb-2 text-secondary mt-8">Chant</h2>
+        <ul class="list-disc pl-4">
+          <li>Conservatoire (2022 - en cours)</li>
+          <li>Chorale ("Nota bene", 2022 - en cours)</li>
+          <li>
+            Cover & compositions (Chaine Youtube & réseaux sociaux, ? - en
+            cours)
+          </li>
+          <li>Scènes ouvertes & soliste lors de spectacles</li>
+        </ul>
+        <h2 class="font-title text-xl  mb-2 text-secondary mt-8">Doublage</h2>
+        <ul class="list-disc pl-4">
+          <li>Doublage voix off (Bubble Flat, 2019)</li>
+          <li>
+            Doublage bande rythmo (Chaine Youtube & réseaux sociaux, ? - en
+            cours)
+          </li>
+          <li>Plus tard Fiver Jeu vidéo</li>
+        </ul>
+        <h2 class="font-title text-xl  mb-2 text-secondary mt-8">Animateur</h2>
+        <ul class="list-disc pl-4">
+          <li>Soirée JDR comme MJ</li>
+          <li>Présentateur et animateur de quizz et soirées jeux</li>
+          <li>Live stream sur Twitch (Vallab) Emissions webs</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
