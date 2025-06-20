9b2033662af2db991a58097ad309273c6f446d03
Jerome <vallabpro@gmail.com>
2025-06-20 15:55:07 +0200

    feat(router): Add router & split by page

---
+++ src/routes/presentation.jsx
@@ -0,0 +1,17 @@
+import { APropos } from "../components/presentation/a-propos";
+import { Atouts } from "../components/presentation/atouts";
+import { Competences } from "../components/presentation/competences";
+import { Forces } from "../components/presentation/forces";
+import { Techniques } from "../components/presentation/techniques";
+
+export function Presentation() {
+  return (
+    <main>
+      {/* <APropos />
+      <Atouts />
+      <Competences />
+      <Forces />
+      <Techniques /> */}
+    </main>
+  );
+}
